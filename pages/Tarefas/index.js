68132e56e9293c06ff6808fdf01c8779dece071e
rhuanbello <rhuambello@gmail.com>
2022-04-12 22:46:22 -0300

    feat: Delete Task | PUT Complete Task

--- pages/Tarefas/index.js
+++ pages/Tarefas/index.js
@@ -1,7 +1,8 @@
 const profileContainer = document.querySelector('.user-info')
 const tasksContainer = document.querySelector('.tasks-container')
+const tasksContainerPending = document.querySelector('.tasks-container-pending')
 
-const createTaskForm = document.querySelector('.nova-tarefa')
+const createTaskForm = document.querySelector('button[type="submit"]')
 const inputDescricaoTarefa = document.querySelector('#novaTarea')
 
 const url = 'https://ctd-todo-api.herokuapp.com/v1'
@@ -28,7 +29,7 @@ const getUser = () => {
     .catch(error => console.log(error))
 }
 
-const renderUsername = ({ firstName, lastName, email}) => {
+const renderUsername = ({ firstName, lastName, email }) => {
   const imgUrl = email.split('@')[0]
 
   profileContainer.innerHTML = `
@@ -44,43 +45,129 @@ const renderUsername = ({ firstName, lastName, email}) => {
 
 const getRemoveTask = () => {
   const tasksElement = document.querySelectorAll('.tarefa')
-  console.log(tasksElement)
 
   tasksElement.forEach((task) => {
-    const removeButton = task.querySelector('i')
-    removeButton.addEventListener('click', () => {
-      console.log('id', task.getAttribute('data-id'))
-    })
+    const removeButton = task.querySelector('.ri-delete-bin-line')
+    if (removeButton) {
+      removeButton.addEventListener('click', () => {
+        const id = task.getAttribute('data-id')
+        deleteTask(id)
+      })
+    }
   })
+}
 
+const deleteTask = (id) => {
+  fetch(`${url}/tasks/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': JSON.parse(sessionStorage.getItem('token')),
+    }
+  })
+    .then(response => response.json())
+    .then(data => getTasks())
+    .catch(error => console.log(error))
+}
+
+const updateTaskStatus = ({ id, description, completed }) => {
+
+  const updateTaskObj = {
+    description: description,
+    completed: !completed
+  }
+
+  fetch(`${url}/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': JSON.parse(sessionStorage.getItem('token')),
+    },
+    body: JSON.stringify(updateTaskObj)
+  })
+    .then(response => response.json())
+    .then(data => getTasks())
+    .catch(error => console.log(error))
+}
+
+const handleFormatDate = (createdAt) => {
+  return (
+    new Date(createdAt).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+  )
 }
 
 const renderTasks = (tasks) => {
   const orderedTasks = tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
 
   tasksContainer.innerHTML = '';
+  tasksContainerPending.innerHTML = '';
 
-  orderedTasks.forEach(({ description, createdAt, id }) => {
-    tasksContainer.innerHTML += `
-      <li class="tarefa" data-id="${id}">
-        <div class="not-done"></div>
-        <div class="descricao">
-          <p class="nome">${description}</p>
-          <p class="timestamp">Criada em: ${new Date(createdAt).toLocaleDateString('pt-BR')}
-          <i class="ri-delete-bin-line"></i>
-          </p>
-        </div>
-      </li>
-    `
+  orderedTasks.forEach(({ description, createdAt, id, completed }) => {
+
+    if (completed) {
+      tasksContainerPending.innerHTML += `
+        <li class="tarefa" data-id="${id}">
+          <div class="done">
+            <button class="toggle-complete">
+              <i class="ri-check-line"></i>
+            </button>
+          </div>
+          <div class="descricao">
+            <p class="nome">${description}</p>
+            <p class="timestamp">Criada em: ${handleFormatDate(createdAt)}
+            <i class="ri-delete-bin-line" ></i >
+            </p>
+          </div>
+        </li>
+      `
+    } else {
+      tasksContainer.innerHTML += `
+        <li class="tarefa" data-id="${id}">
+          <div class="not-done">
+            <button class="toggle-complete">
+              <i class="ri-check-line"></i>
+            </button>
+          </div>
+          <div class="descricao">
+            <p class="nome">${description}</p>
+            <p class="timestamp">Criada em: ${handleFormatDate(createdAt)}
+            </p>
+          </div>
+        </li>
+      `
+    }
+
   })
-
   getRemoveTask()
+  getUpdateTask()
+}
+
+const getUpdateTask = () => {
+  const toggleComplete = document.querySelectorAll('.toggle-complete')
+
+  toggleComplete.forEach((button) => {
+    button.addEventListener('click', () => {
+      const task = button.parentElement.parentElement
+      const id = task.getAttribute('data-id')
+      getTasks(id)
+    })
+  })
 
 }
 
-const getTasks = () => {
+const getTasks = (id) => {
 
-  fetch(`${url}/tasks`, {
+  const isGetById = id ? `/${id}` : '';
+
+  fetch(`${url}/tasks${isGetById}`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -92,10 +179,15 @@ const getTasks = () => {
       return response.json()
     })
     .then((data) => {
-      renderTasks(data)
-      setTimeout(() => {
-        tasksContainer.removeAttribute('id')
-      }, 1000)
+      if (isGetById) {
+        updateTaskStatus(data)
+      } else {
+        renderTasks(data)
+        setTimeout(() => {
+          tasksContainer.removeAttribute('id')
+          tasksContainerPending.removeAttribute('id')
+        }, 1000)
+      }
     })
     .catch(error => console.log(error))
 }
@@ -118,6 +210,7 @@ const createTasks = (description) => {
   })
     .then(response => response.json())
     .then(() => {
+      inputDescricaoTarefa.value = '';
       getTasks()
     })
     .catch(error => console.log(error))
@@ -131,15 +224,20 @@ const initApp = () => {
     location.href = '../../index.html';
   })
 
-  // getTasks();
   getTasks();
-  
 
 }
 
-createTaskForm.addEventListener('submit', (e) => {
+createTaskForm.addEventListener('click', (e) => {
   e.preventDefault()
   createTasks(inputDescricaoTarefa.value)
+})
+
+inputDescricaoTarefa.addEventListener('keypress', (e) => {
+  if (e.key == 'Enter') {
+    e.preventDefault()
+    createTasks(inputDescricaoTarefa.value)
+  } 
 })
 
 window.onload = () => {
